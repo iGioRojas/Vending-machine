0eb5e9d91cca10673df0989a08794e4cef3e4fb7
Stef <68661240+stefa17pj@users.noreply.github.com>
2021-11-14 18:59:24 -0500

    archivo .asm

---
+++ vending-machine.asm
@@ -0,0 +1,18 @@
+.data                        
+
+titulo:        .asciiz "Maquina expendedora de bebidas:3 \n"
+total:          .asciiz "El total es: "
+cambio:         .asciiz "Su cambio es: "
+sel:            .asciiz "Seleccionaste  "
+salto_linea: 	.asciiz "\n"
+
+# Bebidas
+agua:           .asciiz "Agua\n"
+aguacongas:     .asciiz "Agua con Gas\n"
+cocacola:       .asciiz "Coca Cola\n"
+sprite:         .asciiz "Sprite\n"
+fanta:		.asciiz "Fanta \n"
+powerade:       .asciiz "Powerade\n"
+gatorade:	.asciiz "Gatorade \n"
+vivecien:	.asciiz "Vive 100 \n"
+limonada:	.asciiz "Limonada Tesalia"
