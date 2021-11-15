71ec42a69b5931d4507b45890b9cd523d3ba01c8
Stef <68661240+stefa17pj@users.noreply.github.com>
2021-11-14 19:37:21 -0500

    cambios

--- vending-machine.asm
+++ vending-machine.asm
@@ -1,18 +1,32 @@
 .data                        
 
-titulo:        .asciiz "Maquina expendedora de bebidas:3 \n"
+titulo:         .asciiz "Maquina expendedora de bebidas:3 \n"
+sel:            .asciiz "Seleccionaste  "
 total:          .asciiz "El total es: "
 cambio:         .asciiz "Su cambio es: "
-sel:            .asciiz "Seleccionaste  "
-salto_linea: 	.asciiz "\n"
+nostock:        .asciiz "Lo sentimos, la bebida esta agotada"
+salto_linea: 	  .asciiz "\n"
 
-# Bebidas
-agua:           .asciiz "Agua\n"
-aguacongas:     .asciiz "Agua con Gas\n"
-cocacola:       .asciiz "Coca Cola\n"
+# Bebidas en la maquina expendedora
+agua:           .asciiz "Agua \n"
+aguacongas:     .asciiz "Agua con Gas \n"
+cocacola:       .asciiz "Coca Cola \n"
 sprite:         .asciiz "Sprite\n"
-fanta:		.asciiz "Fanta \n"
-powerade:       .asciiz "Powerade\n"
-gatorade:	.asciiz "Gatorade \n"
-vivecien:	.asciiz "Vive 100 \n"
-limonada:	.asciiz "Limonada Tesalia"
+fanta:		      .asciiz "Fanta \n"
+powerade:       .asciiz "Powerade \n"
+gatorade:	      .asciiz "Gatorade \n"
+vivecien:	      .asciiz "Vive 100 \n"
+limonada:	      .asciiz "Limonada Tesalia \n"
+
+#stock inicial de las Bebidas
+aguaCant:       .word 10
+aguagasCant:    .word 10
+cocacolaCant:   .word 10
+spriteCant:     .word 10
+fantaCant:      .word 10
+powerCant:      .word 10
+gatoCant:       .word 10
+vivecienCant:   .word 10
+limonada:       .word 10
+
+#Billetes y monedas
